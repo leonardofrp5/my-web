2325743755fe68d27d383c58e6c75f3a65634f16
leonardofrp5 <leonardofrp5@gmail.com>
2021-04-05 13:15:49 -0500

    added meta in html

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className='container'>
-      <h1 className='title'>Leonardo Romero Perez</h1>
-    </div>
+    <>
+      <div className='container'>
+        <h1 className='title'>Leonardo Romero Perez</h1>
+      </div>
+    </>
   );
 }
 
