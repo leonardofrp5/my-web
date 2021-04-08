37923d3b8d7195b277a0674116892221f9724ff6
leonardofrp5 <leonardofrp5@gmail.com>
2021-04-07 19:25:45 -0500

    added all rules in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,25 +20,34 @@ module.exports = {
   settings: {
     'import/resolver': {
       node: {
-        paths: ['src']
-      }
-    }
+        paths: ['src'],
+      },
+    },
   },
   rules: {
-    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
-    'indent': ['error', 2],
-    'quotes': ['warn', 'single'],
-    // 'no-console': 'off',
-    // 'semi': ['error', 'always'],
-    // comma-dangle": ["error", "always"],
-    'import/no-extraneous-dependencies': 'off'
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    indent: ['error', 2],
+    quotes: ['warn', 'single'],
+    semi: ['error', 'always'],
+    'no-console': 'warn',
+    'eol-last': 'error',
+    eqeqeq: 'error',
+    'no-var': 'off',
+    'no-trailing-spaces': 2,
+    'space-before-blocks': ['warn', { keywords: 'always' }],
+    'arrow-spacing': [2, { before: true, after: true }],
+    'comma-spacing': [2, { before: false, after: true }],
+    'import/no-extraneous-dependencies': 'off',
+    'semi-spacing': 2,
+    'no-mixed-spaces-and-tabs': 1,
+    'object-curly-spacing': [2, 'always'],
+    'no-multiple-empty-lines': [2, { max: 2 }],
+    'jsx-quotes': [1, 'prefer-double'],
+    'react/no-did-mount-set-state': 1,
+    'react/no-did-update-set-state': 1,
+    'react/no-direct-mutation-state': 1,
+    'react/no-unknown-property': 1,
+    'react/self-closing-comp': 1,
+    'react/sort-comp': 2,
   },
-  // overrides: [
-  //   {
-  //     'files': ['src/*.reportWebVitals.js'],
-  //     'rules': {
-  //       'no-unused-expressions': 'off'
-  //     }
-  //   }
-  // ]
-}
+};
