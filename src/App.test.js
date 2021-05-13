52eabcef603562b0951d465bdd6ab4905250c9f9
leonardofrp5 <leonardofrp5@gmail.com>
2021-05-13 10:02:58 -0500

    added packeges standart for styles of code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import App from './App'
+
+describe('#App', () => {
+  test('Check component App', () => {
+    const { container } = render(<App />)
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
