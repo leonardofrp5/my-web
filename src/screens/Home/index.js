84528d29efd2def922e067b761e380da7e3266a1
leonardofrp5 <leonardofrp5@gmail.com>
2021-05-16 16:52:32 -0500

    added mode of theme but need refactory in code

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,34 +1,61 @@
+import { useState } from 'react'
+
+import Footer from 'components/Footer'
+
 import imgProfile from 'assets/Images/imgProfile.png'
 import { ReactComponent as IconGitHub } from 'assets/Images/github.svg'
 import { ReactComponent as IconTwitter } from 'assets/Images/twitter.svg'
 import { ReactComponent as IconInstagram } from 'assets/Images/instagram.svg'
 import { ReactComponent as IconLinkedin } from 'assets/Images/linkedin.svg'
+
 import styles from './styles.module.scss'
 
 function Home () {
+  const [theme, setTheme] = useState('dark')
+  const [statusCheck, setStatusCheck] = useState(false)
+
+  const toggleTheme = () => {
+    setStatusCheck(!statusCheck)
+    const newTheme = statusCheck ? 'dark' : 'light'
+    setTheme(newTheme)
+  }
+
   return (
     <>
-      <div className={styles.container}>
-        <img className={styles.imgProfile} src={imgProfile} alt='img_leonardo' />
-        <h3 className={styles.title}>Leonardo Romero Perez</h3>
+      <div className={`container container--${theme}`}>
+        <div className={styles.switch}>
+          <input
+            type='checkbox'
+            className={styles.switch__input}
+            id='Switch'
+            onChange={() => toggleTheme()}
+          />
+          <label className={styles.switch__label} htmlFor='Switch'>
+            <span className={styles.switch__indicator} />
+            <span className={styles.switch__decoration} />
+          </label>
+        </div>
+        <img className={`container--${theme}--imgProfile`} src={imgProfile} alt='img_leonardo' />
+        <h3 className={`container--${theme}--title`}>Leonardo Romero Perez</h3>
         <div className={styles.containerIcons}>
           <a href='https://github.com/leonardofrp5' target='_blank' rel='noopener noreferrer'>
-            <IconGitHub className={styles.icon} />
+            <IconGitHub className={`container--${theme}--icon`} />
           </a>
           <a
             href='https://www.linkedin.com/in/leonardo-fabian-romero-perez-516479168/'
             target='_blank'
             rel='noopener noreferrer'
           >
-            <IconLinkedin className={styles.icon} />
+            <IconLinkedin className={`container--${theme}--icon`} />
           </a>
           <a href='https://twitter.com/leofrp' target='_blank' rel='noopener noreferrer'>
-            <IconTwitter className={styles.icon} />
+            <IconTwitter className={`container--${theme}--icon`} />
           </a>
           <a href='https://www.instagram.com/dev.leofrp/' target='_blank' rel='noopener noreferrer'>
-            <IconInstagram className={styles.icon} />
+            <IconInstagram className={`container--${theme}--icon`} />
           </a>
         </div>
+        <Footer />
       </div>
     </>
   )
