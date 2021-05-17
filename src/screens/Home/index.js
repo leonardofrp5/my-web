e6cdb0f6a4994108e352a37bd779085c3f5c1076
leonardofrp5 <leonardofrp5@gmail.com>
2021-05-17 13:50:34 -0500

    added text about me

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -37,6 +37,17 @@ function Home () {
         </div>
         <img className={`container--${theme}--imgProfile`} src={imgProfile} alt='img_leonardo' />
         <h3 className={`container--${theme}--title`}>Leonardo Romero Perez</h3>
+        <div className={styles.aboutMe}>
+          <p className={`container--${theme}--paragraph`}>
+            Hi, I'm Leonardo Romero, a passionate self-taught web developer and a system engineer from
+            Colombia. My passion for software lies with dreaming up ideas and making them come true with
+            elegant interfaces, and code quality of the things I build.
+          </p>
+          <p className={`container--${theme}--paragraph`}>
+            I like to work with people that care more for the people and work together for an objectiveI like
+            to work with people that care more for the people and work together for an objective
+          </p>
+        </div>
         <div className={styles.containerIcons}>
           <a href='https://github.com/leonardofrp5' target='_blank' rel='noopener noreferrer'>
             <IconGitHub className={`container--${theme}--icon`} />
